baff5535401d074d2f9d98c4301c82ba5e7fc6ab
Rafael <rafaelproinfo@gmail.com>
2021-09-08 02:56:50 -0300

    Project add Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Olá Mundo!</h1>
+    <div>
+     <h1 className="text-primary">Olá Mundo!</h1>
     </div>
   );
 }
